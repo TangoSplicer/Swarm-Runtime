333f686dc03458303c1462a0f0adecc8a9cf8e31
TangoSplicer <jasonmaoakley@gmail.com>
2026-03-20 13:29:57 +0000

    style: fix rustfmt trailing whitespace in test payload

--- test_payloads/stateful-counter/src/lib.rs
+++ test_payloads/stateful-counter/src/lib.rs
@@ -6,7 +6,7 @@ pub extern "C" fn execute(_ptr: i32, _len: i32) -> i32 {
     unsafe {
         // Increment the stateful counter
         COUNTER += 1;
-        
+
         // Return the new counter value to the Judge
         COUNTER
     }
